134bbec55474f5331e9150157cd472a77b23098b
Alif Arlendi <139869958+alifarlendi@users.noreply.github.com>
2025-02-16 17:18:56 +0700

    Update main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+
+def deposit():
+    while True:
+        amount = input("Enter the amount you want to deposit: $")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
