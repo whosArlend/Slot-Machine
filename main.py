a48142cf8bb1cf5bbea4243c4fae6dd4df7c66a6
Alif Arlendi <139869958+whosArlend@users.noreply.github.com>
2025-02-16 17:20:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -8,3 +8,7 @@ def deposit():
                 break
             else:
                 print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+
+    return amount
