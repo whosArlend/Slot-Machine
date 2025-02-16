92042640a07c78e39598929d7d09a5499818c8fb
Alif Arlendi <139869958+whosArlend@users.noreply.github.com>
2025-02-16 19:54:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+MAX_LINES = 3
+
 
 def deposit():
     while True:
@@ -12,3 +14,23 @@ def deposit():
             print("Please enter a number.")
 
     return amount
+
+def get_number_of_lines():
+    while True:
+        lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")?")
+        if amount.isdigit():
+            amount = int(amount)
+            if amount > 0:
+                break
+            else:
+                print("Amount must be greater than 0.")
+        else:
+            print("Please enter a number.")
+
+    return amount
+
+def main():
+    balance = deposit()
+
+
+main()
