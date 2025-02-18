2b1df178edac7719a754e6d9d7b82e8a6d16283f
Alif Arlendi <139869958+whosArlend@users.noreply.github.com>
2025-02-18 11:32:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 MAX_LINES = 3
-
+MAX_BET = 100
+MIN_BET = 1
 
 def deposit():
     while True:
@@ -18,12 +19,26 @@ def deposit():
 def get_number_of_lines():
     while True:
         lines = input("Enter the number of lines to bet on (1-" + str(MAX_LINES) + ")?")
-        if amount.isdigit():
-            amount = int(amount)
-            if amount > 0:
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINES:
                 break
             else:
-                print("Amount must be greater than 0.")
+                print("Enter a valid number of lines.")
+        else:
+            print("Please enter a number.")
+
+    return lines
+
+def get_bet():
+    while True:
+        amount = input("What would you like to bet on each line? $")
+        if amount.isdigit():
+            amount = int(amount)
+            if MIN_BET <= amount <= MAX_BET:
+                break
+            else:
+                print(f"Amount must be between ${MIN_BET} - ${MAX_BET}.")
         else:
             print("Please enter a number.")
 
@@ -31,6 +46,9 @@ def get_number_of_lines():
 
 def main():
     balance = deposit()
+    lines = get_number_of_lines()
+    bet = get_bet()
+    print(balance, lines)
 
 
 main()
